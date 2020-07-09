d2a137b3c80f82885ff4828f25dd1f5ee6b364aa
BrandyBecker <brandybeckerfsw@gmail.com>
2020-07-09 10:18:12 -0400

    expensive seq complete

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,10 +1,17 @@
-# Your code here
-
+# Your code here ------------ v
+cache_table = {}
+# --------------------------- ^
 
 def expensive_seq(x, y, z):
-    # Your code here
+    # Your code here ------------- v
+    if x <= 0: 
+        return y + z
 
+    if (x, y, z) not in cache_table:
+        cache_table[(x, y, z)] = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
 
+    return cache_table[(x, y, z)]
+    # ---------------------------- ^
 
 if __name__ == "__main__":
     for i in range(10):
